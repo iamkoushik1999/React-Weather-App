4569647cbc5926bccd8731510e259babf8d1c3a7
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-06-27 21:26:23 +0530

    TimeAndLocation Component

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TimeAndLocation = () => {
+  return (
+    <div className=''>
+      <div className='flex items-center justify-center my-6'>
+        <p className='text-white text-xl font-extralight'>
+          Monday, 26 June 2023 | Local Time: 4:00 AM
+        </p>
+      </div>
+      <div className='flex items-center justify-center my-3'>
+        <p className='text-white text-3xl font-medium'>Kolkata, India</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLocation;
