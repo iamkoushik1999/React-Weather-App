b55f7b773fa29f5fb3a5d9d39dd793f0fabc4178
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-06-28 22:10:47 +0530

    weatherService for Current weather data by city

---
+++ src/services/weatherService.js
@@ -0,0 +1,52 @@
+const API_KEY = "f23b34afc9d26a89ac786ccb562c84f9";
+const base_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(base_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrectWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrectWeather);
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
